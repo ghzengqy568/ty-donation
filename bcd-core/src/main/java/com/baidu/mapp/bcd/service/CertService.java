1fc6a9e86f54bc4a8120604a339a0fae7fbcec23
zengqingyong <zengqingyong@baidu.com>
2020-07-21 20:49:55 +0800

    证书

--- bcd-core/src/main/java/com/baidu/mapp/bcd/service/CertService.java
+++ bcd-core/src/main/java/com/baidu/mapp/bcd/service/CertService.java
@@ -6,8 +6,6 @@ package com.baidu.mapp.bcd.service;
 import java.net.URI;
 import java.net.URL;
 import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,14 +13,12 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.baidu.mapp.bcd.common.gson.GsonUtils;
